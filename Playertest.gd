b20464a29fc11df9416c7dcc7978ad27acb105f0
Julian Steiner <juli.steiner@bluewin.ch>
2022-02-21 16:25:26 +0100

    provivsorische szene

--- Playertest.gd
+++ Playertest.gd
@@ -4,13 +4,16 @@ var velocity = Vector2(0, 0)
 
 var speed = 300;
 
-func _ready():
-	velocity.x = Input.is_action_just_pressed("ui_left") - Input.is_action_just_pressed("ui_right");
-	velocity.y = Input.is_action_just_pressed("ui_down") - Input.is_action_just_pressed("ui_up");
-	velocity.normalize()
+#func _ready():
+	
 	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	velocity.x = Input.get_action_raw_strength("ui_right") - Input.get_action_raw_strength("ui_left");
+	velocity.y = Input.get_action_raw_strength("ui_down") - Input.get_action_raw_strength("ui_up");
+	velocity = velocity.normalized();
+	print(velocity)
+	
+	move_and_collide(velocity * speed * delta);
